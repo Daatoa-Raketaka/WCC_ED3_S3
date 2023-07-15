460dfeabe79081201ca11a8f345c75486095b4ab
baibsFar <fararano.kevin@gmail.com>
2023-07-15 15:46:25 +0300

    feat: banner position and text

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,12 @@ const navbarChoiceBtn = document.querySelector('.navbar>.right-side>.portfolio-c
 const navbarChoice = document.querySelector('.navbar>.choice-swipe') as HTMLElement
 
 navbarChoiceBtn.addEventListener('click', () => { navbarChoice.classList.toggle('active') })
+
+/* Custom cursor */
+const customCursor = document.querySelector('.custom-cursor') as HTMLElement
+
+window.addEventListener('mousemove', (e: MouseEvent) => {
+  customCursor.style.left = `${e.clientX}px`
+  customCursor.style.top = `${e.clientY}px`
+  customCursor.style.transform = 'translate(-50%, -50%) rotateZ(45deg)'
+})
