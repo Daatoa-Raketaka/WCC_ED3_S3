59df823f8c560513a2a9fb0093b21229f3410908
baibsFar <fararano.kevin@gmail.com>
2023-07-15 14:38:14 +0300

    feat: navbar, swipe choice

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 import './style.scss'
+
+/* Navbar choice */
+const navbarChoiceBtn = document.querySelector('.navbar>.right-side>.portfolio-choice>.icon') as HTMLElement
+const navbarChoice = document.querySelector('.navbar>.choice-swipe') as HTMLElement
+
+navbarChoiceBtn.addEventListener('click', () => { navbarChoice.classList.toggle('active') })
