e51ce828bddb009c7b2163567b9863c7385d203f
baibsFar <fararano.kevin@gmail.com>
2023-07-16 17:22:27 +0300

    feat: slide choice

--- src/modules/swiper.module.ts
+++ src/modules/swiper.module.ts
@@ -2,12 +2,30 @@ import Swiper from 'swiper/bundle'
 import 'swiper/css/bundle'
 
 export function initSwiper() {
-    new Swiper('.swiper', {
-      // Optional parameters
-      direction: 'horizontal',
-      speed: 500,
-    
-      effect: 'cube',
-      loop: true
-    });
+  const navbarChoiceBtn = document.querySelector('.navbar>.right-side>.portfolio-choice>.icon') as HTMLElement
+  const navbarChoice = document.querySelector('.navbar>.choice-swipe') as HTMLElement
+  const choices = navbarChoice.querySelectorAll('.choice') as NodeList
+  const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 500,
+
+    effect: 'cube',
+    loop: true
+  });
+
+  navbarChoiceBtn.addEventListener('click', () => { navbarChoice.classList.toggle('active') })
+
+  choices.forEach(choice => {
+    (choice as HTMLElement).addEventListener('click', () => {
+      navbarChoice.classList.toggle('active')
+      if (choice === choices[0]){ // Daatoa
+        swiper.slideTo(0, 500)
+      }
+      else if (choice === choices[1]) {
+        swiper.slideTo(1, 500)
+      }
+      window.location.href = '#portfolio'
+    })
+  })
 }
